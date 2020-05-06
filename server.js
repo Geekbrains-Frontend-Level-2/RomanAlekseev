5286c1bd4a1fd89ed05b3d32fd561e9a2c9d6856
MrRoman Alekseev <mrromanalekseev@gmail.com>
2020-05-06 23:30:59 +0300

    fixed

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    const pablicPath = './public'
+    const publicPath = './public'
     console.log(req.url)
 
     const body = req.url === `/css/style.css`
       ? fs.readFileSync(`${publicPath}/css/style.css`, 'utf8')
-      : fs.readFileSync(`${pablicPath}/index.html`,'utf8')
+      : fs.readFileSync(`${publicPath}/index.html`,'utf8')
       res.end(body)
 })
 
