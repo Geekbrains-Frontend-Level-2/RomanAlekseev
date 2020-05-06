8a0c7add762305467908ae7825141674732cd383
MrRoman Alekseev <mrromanalekseev@gmail.com>
2020-05-06 16:15:55 +0300

    ready for heroku
    ready for heroku
    -created samle of eShop
    -server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    const pablicPath = './public'
+    console.log(req.url)
+
+    const body = req.url === '/css/styles.css'
+      ? fs.readFileSync(`${publicPath}/css/style.css`, 'utf8')
+      : fs.readFileSync(`${pablicPath}/index.html`,'utf8')
+      req.end(body)
+})
+
+const port = process.env.PORT || 3000
+server.listen(port)
+console.log('Server started on port: ', port)
