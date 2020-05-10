4f24d8da50f6cd1a9cf9744e67d8c62138a87acd
MrRoman Alekseev <mrromanalekseev@gmail.com>
2020-05-10 11:53:09 +0300

    server fix

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const server = http.createServer((req, res) => {
       : fs.readFileSync(`${publicPath}/index.html`,'utf8')
       res.end(js)
 
-    const text = fs.readFileSync('script.js', 'utf8')
-    console.log(text)
-    const now = new Date().toLocaleString()
-    console.log(now)
-
 })
 
 const port = process.env.PORT || 3000
 server.listen(port)
 console.log('Server started on port: ', port)
+
+const text = fs.readFileSync('script.js', 'utf8')
+console.log(text)
+const now = new Date().toLocaleString()
+console.log(now)
